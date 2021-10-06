78fb37b517e30b8a587e7446cb1b0c4b8283147a
ADCairex <angelmartinezguardiola@gmail.com>
2021-10-06 16:19:37 +0200

    update the clasesPersonajes.php, make the cargarPersonajes.php and make the personajes.json

--- clasesPersonajes.php
+++ clasesPersonajes.php
@@ -4,27 +4,36 @@
         public $id;
         public $nombre;
         public $dni;
+        public $edad;
         public $altura;
         public $peso;
         public $imagen;
         public $icono;
         public $descripcion;
+
+        function calcularEdad() {
+            
+        }
     }
 
     class Orco extends Personaje {
-
+        public $colmillos;
+        public $raza;
     }
     
     class Elfo extends Personaje {
-        
+        public $orejas;
+        public $clan;
     }
 
     class Hombre extends Personaje {
-        
+        public $raza;
+        public $familia;
     }
 
     class Enano extends Personaje {
-        
+        public $barba;
+        public $herrero;
     }
 
 ?>
