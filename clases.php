73f0244d81a3de74c4c91068984ca29075d3e92c
ADCairex <angelmartinezguardiola@gmail.com>
2021-10-06 15:27:16 +0200

    create the clases

---
+++ clases.php
@@ -0,0 +1,30 @@
+<?php
+
+    class Personaje {
+        public $id;
+        public $nombre;
+        public $dni;
+        public $altura;
+        public $peso;
+        public $imagen;
+        public $icono;
+        public $descripcion;
+    }
+
+    class Orco extends Personaje {
+
+    }
+    
+    class Elfo extends Personaje {
+        
+    }
+
+    class Hombre extends Personaje {
+        
+    }
+
+    class Enano extends Personaje {
+        
+    }
+
+?>
