f36e798f306c92bb1c0450b30bd8377fa143a9d7
desvillena <desarrollovillena@gmail.com>
2021-10-06 17:04:44 +0200

    Cambio clasesEquipamiento.php

--- clasesEquipamiento.php
+++ clasesEquipamiento.php
@@ -1,37 +1,96 @@
 <?php
 
     class Equipamiento {
+
         public $id;
         public $nombre;
         public $antiguedad;
-        public $dimension_largo;
-        public $dimension_ancho;
+        public $dimensionLargo;
+        public $dimensionAncho;
         public $peso;
         public $imagen;
         public $icono;
         public $descripcion;
+
+        function __construct($id, $nombre, $antiguedad, $dimension_largo, $dimension_ancho, $peso, $imagen, $icono, $descripcion){
+
+            $this->id = $id;
+            $this->nombre = $nombre;
+            $this->antiguedad = $antiguedad;
+            $this->dimensionLargo = $dimensionLargo;
+            $this->dimensionAncho = $dimensionAncho;
+            $this->peso= $peso;
+            $this->imagen = $imagen;
+            $this->icono= $icono;
+            $this->descripcion = $descripcion;
+        }
+
+        function setId($id) {
+            $this->id = $id;
+        }
+        function setName($nombre) {
+            $this->nombre = $nombre;
+        }
+    
+        function getId() {
+            return $this->id;
+        }
+        function getName() {
+            return $this->nombre;
+        }
+       
     }
 
     class Espada extends Equipamiento {
-        public $material_hoja;
-        public $material_mango;
+
+        public $materialHoja;
+        public $materialMango;
+
+        function __construct($id, $nombre, $antiguedad, $dimension_largo, $dimension_ancho, $peso, $imagen, $icono, $descripcion, $materialHoja, $materialMango ){
+            parent::__construct($id, $nombre, $antiguedad, $dimension_largo, $dimension_ancho, $peso, $imagen, $icono, $descripcion);
+    
+            $this->materialHoja = $materialHoja;
+            $this->materialMango = $materialMango;
+        }
 
     }
     
     class Arco extends Equipamiento {
-        public $material_cuerpo;
-        public $material_cuerda;
+
+        public $materialCuerpo;
+        public $materialCuerda;
+
+        function __construct($id, $nombre, $antiguedad, $dimension_largo, $dimension_ancho, $peso, $imagen, $icono, $descripcion, $materialCuerpo, $materialCuerda ){
+            parent::__construct($id, $nombre, $antiguedad, $dimension_largo, $dimension_ancho, $peso, $imagen, $icono, $descripcion);
+    
+            $this->materialCuerpo = $materialCuerpo;
+            $this->materialCuerda = $materialCuerda;
+        }
     }
 
     class Casco extends Equipamiento {
         public $material;
         public $resistencia;
+
+        function __construct($id, $nombre, $antiguedad, $dimension_largo, $dimension_ancho, $peso, $imagen, $icono, $descripcion, $material, $resistencia ){
+            parent::__construct($id, $nombre, $antiguedad, $dimension_largo, $dimension_ancho, $peso, $imagen, $icono, $descripcion);
+    
+            $this->material = $material;
+            $this->resistencia = $resistencia;
+        }
     }
 
     class Hacha extends Equipamiento {
-        public $material_hoja;
-        public $material_mango;
+
+        public $materialHojaH;
+        public $materialMangoH;
        
+        function __construct($id, $nombre, $antiguedad, $dimension_largo, $dimension_ancho, $peso, $imagen, $icono, $descripcion, $materialHojaH, $materialMangoH ){
+            parent::__construct($id, $nombre, $antiguedad, $dimension_largo, $dimension_ancho, $peso, $imagen, $icono, $descripcion);
+    
+            $this->materialHojaH = $materialHojaH;
+            $this->materialMangoH = $materialMangoH;
+        }
     }
 
 ?>
