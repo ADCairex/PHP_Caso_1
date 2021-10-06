82cf1337ca52b2156a7d0524f398f74d04b18a12
ADCairex <angelmartinezguardiola@gmail.com>
2021-10-06 18:47:12 +0200

    finish the header of the index.php and preguntasFrecuentes.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Inicio PHP Caso 1</title>
+        <link rel="stylesheet" href="css/styleIndex.css">
+    </head>
+    <body>
+        <div class="header">
+            <div class="iconTittle">
+                <div>
+                    <img src="imagenes/tittleImage.png" width="50px" height="50px">
+                </div>
+                <div>
+                    <h1 class="headerTittle">Pagina de inicio de PHP Caso 1</h1>
+                </div>
+            </div>
+            <div class="navDiv">
+                <ul class="nav">
+                    <li><a href="index.php">Inicio</a></li>
+                    <li><a href="preguntasFrecuentes.php">Preguentas Frecuentes</a></li>
+                    <li><a href="login.php">Cerrar sesion</a></li>
+                </ul>
+            </div>
+        </div>
+    </body>
+</html>
