47de35a90f260df71d7f2f200ce1ab9a80acf15c
ADCairex <angelmartinezguardiola@gmail.com>
2021-10-08 20:39:17 +0200

    x

--- index.php
+++ index.php
@@ -9,27 +9,9 @@
     </head>
     <body>
         <?php
-
-
             include 'templates/header.php';
             include 'templates/personajes.php';
-
-            $rutaActual = getcwd();
-echo $rutaActual;
-$rutaActualModificada = $rutaActual.DIRECTORY_SEPARATOR.'src';
- 
-//sacamos por pantalla la ruta final
-echo $rutaActualModificada;
-
-
-$rutaActual = getcwd();
-$rutaActualModificada = str_replace("/", $rutaActual);
- 
-//sacamos por pantalla la ruta final
-echo $rutaActualModificada;
             include 'templates/equipamiento.php';
-
-
             include 'templates/footer.php';
             include 'scripts.php';
         ?>
